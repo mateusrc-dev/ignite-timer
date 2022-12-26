bb267dc97141559a49205e04b00032fab19f8127
Mateus Carvalho <mateus_raimundo95@outlook.com>
2022-12-26 16:49:00 -0300

    create page Home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,48 @@
+import { Play } from 'phosphor-react'
+import {
+  CountDownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from './styles'
+
+export function Home() {
+  return (
+    // vamos usar label para quando a pessoa clicar no label dê foco no input - como o button é do tipo submit, a tag 'form' tem que ficar envolvendo ele
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            type="text"
+            placeholder="Dê um nome para o seu projeto"
+          />
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+          />
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountDownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountDownContainer>
+
+        <StartCountdownButton disabled type="submit">
+          <Play size={24} />
+          Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  )
+}
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,7 +10,7 @@ export const GlobalStyle = createGlobalStyle` //vamos colocar aqui todo o CSS qu
   :focus {
     outline: 0; //por padrão o focus é definido pelo outline no navegador, estamos tirando isso
     box-shadow: 0 0 0 2px ${(props) =>
-      props.theme['gray-500']} //o tamanho vai ser 2px, tiramos o blur
+      props.theme['green-500']} //o tamanho vai ser 2px, tiramos o blur
   }
 
   body {
@@ -19,6 +19,7 @@ export const GlobalStyle = createGlobalStyle` //vamos colocar aqui todo o CSS qu
         'gray-900'
       ]}; //vamos usar as cores que estão definidas no nosso tema - ao colocar 'theme.' aparece todas as propriedades que podemos utilizar - devido o traço na variável a sintaxe tem que ser com cochetes
     color: ${(props) => props.theme['gray-300']};  
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
