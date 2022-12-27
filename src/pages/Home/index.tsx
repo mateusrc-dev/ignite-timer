b1d1787b88648e3e77daf779a95d3a082948964d
Mateus Carvalho <mateus_raimundo95@outlook.com>
2022-12-27 16:00:19 -0300

    update page Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,8 @@
 import { Play } from 'phosphor-react'
 // import { useState } from 'react'
 import { useForm } from 'react-hook-form' // vamos importar da biblioteca que lida com formulários
+import { zodResolver } from '@hookform/resolvers/zod' // biblioteca que instalamos pra fazer a integração - temos uma integração específica para o zod
+import * as zod from 'zod' // vamos importar tudo de zod e dar um nome pra isso 'vai ser zod' - isso porque essa biblioteca não tem um export default
 import {
   CountDownContainer,
   FormContainer,
@@ -11,15 +13,28 @@ import {
   TaskInput,
 } from './styles'
 
+const newCycleFormValidationSchema = zod.object({
+  // vamos criar um esquema para validar os dados do nosso formulário - vamos usar object porque vamos validar um objeto
+  task: zod.string().min(1, 'Informe a tarefa!'), // vamos colocar o tipo de dado, o mínimo de caracteres e a mensagem de validação
+  MinutesAmount: zod
+    .number()
+    .min(5, 'O ciclo precisa ser de no mínimo 5 minutos!')
+    .max(60, 'O ciclo precisa ser de no máximo 60 minutos!'),
+})
+
 export function Home() {
-  const { register, handleSubmit, watch } = useForm() // useForm retorna um objeto com várias funções dentro dele - por isso podemos desestruturar - quando usamos o useForm() é como se estivessemos criando um novo formulário na aplicação e a função register fala quais campos vou ter no formulário - register recebe o nome do input (é uma função que recebe parâmetros) e retorna alguns métodos que usamos pra trabalhar com input (onChange, onBlur, onFocus...) - vamos importar a função watch para observar determinado input
+  const { register, handleSubmit, watch } = useForm({
+    resolver: zodResolver(newCycleFormValidationSchema),
+  }) // useForm retorna um objeto com várias funções dentro dele - por isso podemos desestruturar - quando usamos o useForm() é como se estivessemos criando um novo formulário na aplicação e a função register fala quais campos vou ter no formulário - register recebe o nome do input (é uma função que recebe parâmetros) e retorna alguns métodos que usamos pra trabalhar com input (onChange, onBlur, onFocus...) - vamos importar a função watch para observar determinado input - no objeto de configurações vamos colocar o esquema de validação (regras de validação - vamos criar um objeto para colocar essas regras)
 
   function handleCreateNewCycle(data: any) {
     // vamos usar um método com um nome diferente de handleSubmit que estamos pegando acima de useForm - vamos colocar esse método dentro de handleSubmit abaixo - podemos receber como argumento o data (são os dados do nosso input do nosso formulário)
     console.log(data)
   }
 
-  const task = watch('task') // vamos observar o input de nome 'task' (nome que colocamos dentro de register) em tempo real, com isso podemos fazer a validação em 'disabled'
+  // console.log(formState.errors) // no formState tem nossas mensagens de erro, podemos importar formState do useForm()
+
+  const task = watch('task') // vamos observar o input de nome 'task' (nome que colocamos dentro de register) em tempo real, com isso podemos fazer a validação em 'disabled' - transforma o nosso formulário em um controlled
   const isSubmitDisabled = !task
 
   return (
