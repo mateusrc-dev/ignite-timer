64097224567c356e05290dae0b08106004fc8bf7
Mateus Carvalho <mateus_raimundo95@outlook.com>
2022-12-30 10:31:14 -0300

    project finished

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -27,7 +27,7 @@ export function History() {
                   <td>{cycle.task}</td>
                   <td>{cycle.minutesAmount} minutos</td>
                   <td>
-                    {formatDistanceToNow(cycle.startDate, {
+                    {formatDistanceToNow(new Date(cycle.startDate), {
                       addSuffix: true, // para ficar um 'há' na frente
                       locale: ptBR, // para ficar no idioma português
                     })}
